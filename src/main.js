8cb679d946e04e613a7ab338b8a732e1c46532d7
huoge <1102194607@qq.com>
2017-03-28 17:57:40 +0800

    Build the basic structure

--- src/main.js
+++ src/main.js
@@ -2,14 +2,31 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import goods from  './components/goods/goods';
+import ratings from  './components/ratings/ratings';
+import seller from  './components/seller/seller';
 
-Vue.config.productionTip = false;
+import './common/stylus/index.styl';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+Vue.config.productionTip = true;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+const router = new VueRouter({
+  routes,
+  linkActiveClass:'active'
 });
+
+const aapp = new app({
+  router
+}).$mount('#app');
+router.push('/goods');
